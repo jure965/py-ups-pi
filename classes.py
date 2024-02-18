376f4b59763b5da04f7cc1990b5eee4f8f5c52ee
Jure <jure.vengust@gmail.com>
2024-02-18 19:57:54 +0100

    feat: add typing on init functions

--- classes.py
+++ classes.py
@@ -1,19 +1,34 @@
 class WakeConfig:
-    def __init__(self, batt_level_gte, runtime_gte, ups_status_str):
+    def __init__(
+            self,
+            batt_level_gte: int,
+            runtime_gte: int,
+            ups_status_str: str,
+    ):
         self.batt_level_gte = batt_level_gte
         self.runtime_gte = runtime_gte
         self.ups_status_str = ups_status_str
 
 
 class UPSStatus:
-    def __init__(self, battery_charge, battery_runtime, ups_status_str):
+    def __init__(
+            self,
+            battery_charge: int,
+            battery_runtime: int,
+            ups_status_str: str,
+    ):
         self.battery_charge = battery_charge
         self.battery_runtime = battery_runtime
         self.ups_status_str = ups_status_str
 
 
 class Host:
-    def __init__(self, name, mac, address):
+    def __init__(
+            self,
+            name: str,
+            mac: str,
+            address: str,
+    ):
         self.name = name
         self.mac = mac
         self.address = address
